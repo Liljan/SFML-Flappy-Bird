cab14721e619fc91e1edf46907bd62c03a9019f3
Liljan <marcus.t.lilja@gmail.com>
2020-09-14 20:54:05 +0200

    SFML window.

--- Flappy/src/Flappy.cpp
+++ Flappy/src/Flappy.cpp
@@ -1,5 +1,24 @@
+#include <SFML/Graphics.hpp>
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720 
 
 int main()
 {
+	sf::RenderWindow window;
+	window.create(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Fläppi Börd");
+
+	while(window.isOpen())
+	{
+		// Handle events
+		// Update
+		// Render
+
+		window.clear(sf::Color::Black);
+
+		window.display();
+	}
+
+
 	return 0;
 }
